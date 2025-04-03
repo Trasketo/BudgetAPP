d8eda0f586357c9ef246b748b75a3144f189aeda
Marcus Vinícius <mvp.paulo@gmail.com>
2025-04-02 21:31:39 -0400

    Atualiza comentário de teste na classe Main

--- src/main/java/com/BudgetApp/Main.java
+++ src/main/java/com/BudgetApp/Main.java
@@ -17,7 +17,7 @@ public class Main {
         String url = "jdbc:mysql://192.168.12.88:3306";
         String username = "Trasketo";
         String password = "your_password";
-        // teste comment53
+        // teste comment53tes
 
        DatabaseConfig databaseConfig = new DatabaseConfig();
         databaseConfig.setDatabaseUrl("jdbc:mysql://192.168.12.88:3306");
